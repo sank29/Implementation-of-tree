228e5074b47a922a32563fa94a21576cbdf9a58b
sanket <wankhdes29@gmail.com>
2022-05-29 15:15:52 +0530

    insert function added

--- tree.js
+++ tree.js
@@ -14,8 +14,27 @@ class BinarySearchTree {
 
     if (!this.root) {
       this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          // Left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          // right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
     }
-    return this;
   }
   lookup(value) {}
 }
@@ -23,7 +42,10 @@ class BinarySearchTree {
 let tree = new BinarySearchTree();
 console.log(tree.insert(20));
 console.log(tree.insert(40));
-console.log(tree.insert(10));
+console.log(tree.insert(60));
+console.log(tree.insert(50));
+
+JSON.stringify(traverse(tree.root));
 
 function traverse(node) {
   const tree = { value: node.value };
