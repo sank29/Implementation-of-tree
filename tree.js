e7cf6957f5ef54fe8126fe97ad9c160252c04a98
sanket <wankhdes29@gmail.com>
2022-05-29 14:42:49 +0530

    created newNode class

---
+++ tree.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+    }
+    return this;
+  }
+  lookup(value) {}
+}
+
+let tree = new BinarySearchTree();
+console.log(tree.insert(20));
+console.log(tree.insert(40));
+console.log(tree.insert(10));
+
+
+function traverse(node){
+  const tree
+}
